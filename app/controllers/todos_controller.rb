042f363c3c577beb576d6a0944a746e619f0f403
smootysmart <noppakrit.niti@mail.kmutt.ac.th>
2024-11-02 14:38:49 +0700

    [feat] skip each page item

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -25,7 +25,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: "Todo was successfully created." }
+        format.html { redirect_to todos_path, notice: "Todo was successfully created." }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new, status: :unprocessable_entity }
